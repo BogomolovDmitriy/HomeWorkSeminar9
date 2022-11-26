2426bf559c0d012f21584fae1401e69217d25943
Dmitriy <dimgod80@yandex.ru>
2022-11-26 17:05:28 +0800

    add Exercise66

---
+++ Exercise66/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+int Number(int num1, int num2)
+{
+    if (num1 > num2 )
+    {
+        return 0;
+    }
+    
+    return num1 + Number(num1 + 1, num2);
+}
+
+Console.WriteLine(Number(1, 15));
